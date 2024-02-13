54a25a20d5a93e1d44d84e14947c9c5511ff1213
Tijesuni333 <99971409+Tijesuni333@users.noreply.github.com>
2024-02-13 17:13:23 +0100

    new commit

--- TODO smart contract/smart contract/test/Lock.ts
+++ TODO smart contract/smart contract/test/Lock.ts
@@ -7,9 +7,6 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("To do", function () {
-  // We define a fixture to reuse the same setup in every test.
-  // We use loadFixture to run this setup once, snapshot that state,
-  // and reset Hardhat Network to that snapshot in every test.
   async function deployTodoList() {
     const ToDo = await ethers.getContractFactory("ToDo");
     const todo = await ToDo.deploy();
@@ -27,16 +24,52 @@ describe("To do", function () {
       expect(todoList).to.equal("teejay","a boy");
     });
   });
-});
 
-    describe("toggling todo item", function () {
-      it("Should be able to toogle todo item boolean state", async function () {
-        const { todo } = await loadFixture(deployTodoList);
+  describe("toggling todo item", function () {
+    it("Should be able to toogle todo item boolean state", async function () {
+      const { todo } = await loadFixture(deployTodoList);
 
-        const toggle = await todo.toggleTodoItem("iscompleted")
+      const toggle = await todo.toggleTodoItem("iscompleted")
 
-        expect(toggle).to.equal("iscompleted");
+      expect(toggle).to.equal("iscompleted");
       });
     });
 
+    describe("update todo item", function () {
+      it("Should be able to update todo item", async function () {
+        const { todo } = await loadFixture(deployTodoList);
+  
+        const update = await todo.updateTodoItem( 3,"cleaning","the house");
+  
+        expect(update).to.equal("cleaning","the house");
+        });
+      });
+
+      describe("delete todo item", function () {
+        it("Should be able to delete todo item", async function () {
+          const { todo } = await loadFixture(deployTodoList);
+    
+          const deleteItem = await todo.deleteTodoItem(0);
+    
+          expect(deleteItem).to.equal(0);
+          });
+        });
+
+
+      describe("get todo item", function () {
+        it("Should be able to get todo item", async function () {
+          const { todo } = await loadFixture(deployTodoList);
+    
+          const getItem = await todo.getTodoItem(1);
+    
+          expect(getItem).to.equal(1);
+          });
+        });
+});
+
+
+
+
+
+ 
       
